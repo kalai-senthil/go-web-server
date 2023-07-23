13e499b99bcbaada12afbb5c7cb53e0e32825789
Kalaiarasan Senthil <kalaiarasan70810@gmail.com>
2023-07-23 19:20:29 +0530

    server-initialize

---
+++ json.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Fatalf("Server error: %s", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal("unable to write")
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("Checking")
+	godotenv.Load()
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("Specify port")
+		return
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+		MaxAge:           300,
+		ExposedHeaders:   []string{"Link"},
+	}))
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		respondWithJSON(w, 200, struct{}{})
+	})
+	server := &http.Server{
+		Handler: router,
+		Addr:    fmt.Sprintf(": %s", PORT),
+	}
+	log.Printf("Server Running on PORT: %s", PORT)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
